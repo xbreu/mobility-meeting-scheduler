cd3553dac2fdde2692dac0aea9e9baf2ebeb2c55
Alexandre Abreu <up201800168@up.pt>
2022-05-10 16:53:38 +0100

    add: function for filtering json

--- source/sicstus/database.pl
+++ source/sicstus/database.pl
@@ -3,15 +3,27 @@
 object_attribute_value(json(J), K, V) :-
     member(K=V, J).
 
-read_database(data(Fs)) :-
-    read_flights_json(Fs).
+main :-
+    read_flights_json(Rs),
+    % select_all(data(Rs), origin, Vs),
+    % print(Vs),
+    filter_attributes(data(Rs), [zagreb], origin, data(Rfs)),
+    select_all(data(Rfs), origin, Vfs).
 
-select_all(data([]), _, Vs) :- !.
-select_all(data([R | Rs]), K, Vs) :-
-    object_attribute_value(R, K, V),
-    member(V, Vs),
-    select_all(data(Rs), K, Vs).
+select_all(data(Rs), K, Vs) :-
+    setof(V, R^(
+        member(R, Rs),
+        object_attribute_value(R, K, V)
+    ), Vs).
 
-% Selects all value of a key in the entire flight database
-select_all(Rs, K, Vs) :-
-    select_all(Rs, K, Vs).
+filter_attributes(data(D), O, K, data(Df)) :-
+    findall(X, (
+        member(X, D),
+        object_attribute_value(X, K, V),
+        member(V, O)
+    ), Df).
+
+print_elements([]) :- !.
+print_elements([H | T]) :-
+    print(H), nl,
+    print_elements(T).
