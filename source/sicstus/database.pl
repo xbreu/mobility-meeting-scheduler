29de950704099f017c41aa8ab0f82d31493c8481
Alexandre Abreu <up201800168@up.pt>
2022-05-19 22:18:44 +0100

    add: trip object with parsing

--- source/sicstus/database.pl
+++ source/sicstus/database.pl
@@ -1,27 +1,26 @@
 :- consult('./input.pl').
 
-object_attribute_value(json(J), K, V) :-
-    member(K=V, J).
+% -----------------------------------------------------------------------------
+% Trip structure
+% -----------------------------------------------------------------------------
+
+trip_origin(trip(Origin, _, _, _, _, _, _), Origin).
+trip_destination(trip(_, Destination, _, _, _, _, _), Destination).
+trip_departure(trip(_, _, Departure, _, _, _, _), Departure).
+trip_arrival(trip(_, _, _, Arrival, _, _, _), Arrival).
+trip_duration(trip(_, _, _, _, Duration, _, _), Duration).
+trip_price(trip(_, _, _, _, _, Price, _), Price).
+trip_stops(trip(_, _, _, _, _, _, Stops), Stops).
 
 main :-
-    read_flights_json(Rs),
-    % select_all(data(Rs), origin, Vs),
-    % print(Vs),
-    filter_attributes(data(Rs), [zagreb], origin, data(Rfs)),
-    select_all(data(Rfs), origin, Vfs).
+    read_flights_json(J),
+    json_to_list_of_trips(J, Ts),
+    % print_elements(Ts),
+    nth1(1, Ts, T),
+    print(T), nl.
 
-select_all(data(Rs), K, Vs) :-
-    setof(V, R^(
-        member(R, Rs),
-        object_attribute_value(R, K, V)
-    ), Vs).
-
-filter_attributes(data(D), O, K, data(Df)) :-
-    findall(X, (
-        member(X, D),
-        object_attribute_value(X, K, V),
-        member(V, O)
-    ), Df).
+% student("budapest", ["05/05/2022", "06/05/2022"], 1, 600, "05:30:00", "23:30:00")
+% trip("wien", "zagreb", "09/05/2022, 10:10:00", "09/05/2022, 10:55:00", 45, 212, 0)
 
 print_elements([]) :- !.
 print_elements([H | T]) :-
--- source/sicstus/input.pl
+++ source/sicstus/input.pl
@@ -1,6 +1,10 @@
 :- use_module(library(json)).
 :- use_module(library(lists)).
 
+% -----------------------------------------------------------------------------
+% Reading from files
+% -----------------------------------------------------------------------------
+
 eof(-1).
 
 read_until_eof(Content, Content) :-
@@ -25,3 +29,77 @@ read_json_file(F, R) :-
 
 read_flights_json(R) :-
     read_json_file('data/flights.json', R).
+
+% -----------------------------------------------------------------------------
+% Datetime parsing
+% -----------------------------------------------------------------------------
+
+digits_to_number(L, N) :-
+    digits_to_number(L, 0, N).
+
+digits_to_number([], N, N).
+digits_to_number([H | T], A, N) :-
+    B is A * 10 + H,
+    digits_to_number(T, B, N).
+
+datetime(date(Y, M, D), time(Hs, Ms, Ss)) -->
+    date(Y, M, D), ", ", time(Hs, Ms, Ss).
+
+date(Y, M, D) --> number(D), "/", number(M), "/", number(Y).
+
+time(Hs, Ms, Ss) --> number(Hs), ":", number(Ms), ":", number(Ss).
+
+number(N) --> digits(Ds), {digits_to_number(Ds, N)}.
+
+digits([D]) --> digit(D).
+digits([D | N]) --> digit(D), digits(N).
+
+digit(X) --> [C], {"0" =< C, C =< "9", X is C - "0"}.
+
+chars_codes([], []).
+chars_codes([H | T], [C | Cs]) :-
+    char_code(H, C),
+    chars_codes(T, Cs).
+
+atom_string(A, S) :-
+    atom_chars(A, C),
+    chars_codes(C, S).
+
+atom_to_number(A, N) :-
+    atom_string(A, S),
+    phrase(number(N), S).
+
+atom_to_datetime(A, datetime(date(Y, M, D), time(Hs, Ms, Ss))) :-
+    atom_string(A, S),
+    phrase(datetime(date(Y, M, D), time(Hs, Ms, Ss)), S).
+
+atom_to_time(A, time(Hs, Ms, Ss)) :-
+    atom_string(A, S),
+    phrase(time(Hs, Ms, Ss), S).
+
+% -----------------------------------------------------------------------------
+% Json to PROLOG's native structures
+% -----------------------------------------------------------------------------
+
+object_attribute_value(json(J), K, V) :-
+    member(K=V, J).
+
+json_to_list_of_trips([], []).
+json_to_list_of_trips([J | Js], [T | Ts]) :-
+    json_to_trip(J, T),
+    json_to_list_of_trips(Js, Ts).
+
+json_to_trip(J,
+    trip(Origin, Destination, Departure, Arrival, Duration, Price, Stops)) :-
+    object_attribute_value(J, origin, Origin),
+    object_attribute_value(J, destination, Destination),
+    object_attribute_value(J, departure, DepartureAtom),
+    object_attribute_value(J, arrival, ArrivalAtom),
+    object_attribute_value(J, duration, Duration),
+    object_attribute_value(J, price, PriceAtom),
+    object_attribute_value(J, stops, Stops),
+
+    % Parsing of some attributes
+    atom_to_datetime(DepartureAtom, Departure),
+    atom_to_datetime(ArrivalAtom, Arrival),
+    atom_to_number(PriceAtom, Price).
