92c79f5d2026ead56791a9d5e589fceb601651fd
Alexandre Abreu <xbreu@tuta.io>
2022-05-19 23:05:38 +0100

    add: datetime attribute getters

--- source/sicstus/data-structures.pl
+++ source/sicstus/data-structures.pl
@@ -1,6 +1,41 @@
 :- consult('./input.pl').
 
 % -----------------------------------------------------------------------------
+% Date structure
+% -----------------------------------------------------------------------------
+
+% date(Y, M, D)
+
+date_year(date(Y, _, _), Y).
+date_month(date(_, M, _), M).
+date_day(date(_, _, D), D).
+
+% -----------------------------------------------------------------------------
+% Time structure
+% -----------------------------------------------------------------------------
+
+% time(Hs, Ms, Ss)
+
+time_hours(time(H, _, _), H).
+time_minutes(time(_, M, _), M).
+time_seconds(time(_, _, S), S).
+
+% -----------------------------------------------------------------------------
+% Datetime structure
+% -----------------------------------------------------------------------------
+
+% datetime(date(Y, M, D), time(Hs, Ms, Ss))
+
+datetime_date(datetime(D, _), D).
+datetime_time(datetime(_, T), T).
+datetime_year(datetime(date(Y, _, _), _), Y).
+datetime_month(datetime(date(_, M, _), _), M).
+datetime_day(datetime(date(_, _, D), _), D).
+datetime_hours(datetime(_, time(H, _, _)), H).
+datetime_minutes(datetime(_, time(_, M, _)), M).
+datetime_seconds(datetime(_, time(_, _, S)), S).
+
+% -----------------------------------------------------------------------------
 % Trip structure
 % -----------------------------------------------------------------------------
 
@@ -56,3 +91,17 @@ restrict_lengths([], _).
 restrict_lengths([H | T], N) :-
     length(H, N),
     restrict_lengths(T, N).
+
+% -----------------------------------------------------------------------------
+% Structure combinations
+% -----------------------------------------------------------------------------
+
+data_plan_outgoing_trip(Plan, Data, Trip) :-
+    plan_outgoing_trip_index(Plan, O),
+    data_trips(Data, Trips),
+    nth1(O, Trips, Trip).
+
+data_plan_incoming_trip(Data, Plan, Trip) :-
+    plan_outgoing_trip_index(Plan, I),
+    data_trips(Data, Trips),
+    nth1(I, Trips, Trip).
