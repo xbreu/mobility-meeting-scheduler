08ae3e9ee18ab74460b3ebeb7d68aab4cb2b7fa1
Alexandre Abreu <xbreu@tuta.io>
2022-05-19 22:47:34 +0100

    add: parsing of all json files

--- source/sicstus/input.pl
+++ source/sicstus/input.pl
@@ -30,6 +30,9 @@ read_json_file(F, R) :-
 read_flights_json(R) :-
     read_json_file('data/flights.json', R).
 
+read_students_json(R) :-
+    read_json_file('data/students.json', R).
+
 % -----------------------------------------------------------------------------
 % Datetime parsing
 % -----------------------------------------------------------------------------
@@ -43,7 +46,7 @@ digits_to_number([H | T], A, N) :-
     digits_to_number(T, B, N).
 
 datetime(date(Y, M, D), time(Hs, Ms, Ss)) -->
-    date(Y, M, D), ", ", time(Hs, Ms, Ss).
+    date(Y, M, D), ", ", time(Hs, Ms, Ss), " ".
 
 date(Y, M, D) --> number(D), "/", number(M), "/", number(Y).
 
@@ -73,9 +76,18 @@ atom_to_datetime(A, datetime(date(Y, M, D), time(Hs, Ms, Ss))) :-
     atom_string(A, S),
     phrase(datetime(date(Y, M, D), time(Hs, Ms, Ss)), S).
 
+atom_to_date(A, date(Y, M, D)) :-
+    atom_string(A, S),
+    phrase(date(Y, M, D), S).
+
 atom_to_time(A, time(Hs, Ms, Ss)) :-
     atom_string(A, S),
     phrase(time(Hs, Ms, Ss), S).
+
+list_of_atoms_to_list_of_dates([], []).
+list_of_atoms_to_list_of_dates([A | As], [D | Ds]) :-
+    atom_to_date(A, D),
+    list_of_atoms_to_list_of_dates(As, Ds).
 
 % -----------------------------------------------------------------------------
 % Json to PROLOG's native structures
@@ -88,6 +100,11 @@ json_to_list_of_trips([], []).
 json_to_list_of_trips([J | Js], [T | Ts]) :-
     json_to_trip(J, T),
     json_to_list_of_trips(Js, Ts).
+
+json_to_list_of_students([], []).
+json_to_list_of_students([J | Js], [S | Ss]) :-
+    json_to_student(J, S),
+    json_to_list_of_students(Js, Ss).
 
 json_to_trip(J,
     trip(Origin, Destination, Departure, Arrival, Duration, Price, Stops)) :-
@@ -103,3 +120,30 @@ json_to_trip(J,
     atom_to_datetime(DepartureAtom, Departure),
     atom_to_datetime(ArrivalAtom, Arrival),
     atom_to_number(PriceAtom, Price).
+
+json_to_student(J, student(City, Availability, MaxConnections, MaxDuration,
+    EarliestDeparture, LatestArrival)) :-
+    object_attribute_value(J, city, City),
+    object_attribute_value(J, availability, AvailabilityAtoms),
+    object_attribute_value(J, maxConnections, MaxConnections),
+    object_attribute_value(J, maxDuration, MaxDuration),
+    object_attribute_value(J, earliestDeparture, EarliestDepartureAtom),
+    object_attribute_value(J, latestArrival, LatestArrivalAtom),
+
+    % Parsing of some attributes
+    list_of_atoms_to_list_of_dates(AvailabilityAtoms, Availability),
+    atom_to_time(EarliestDepartureAtom, EarliestDeparture),
+    atom_to_time(LatestArrivalAtom, LatestArrival).
+
+% -----------------------------------------------------------------------------
+% Program input
+% -----------------------------------------------------------------------------
+
+read_data(data(Trips, Destinations, Students, MinimumUsefulTime)) :-
+    read_flights_json(Jf),
+    read_students_json(Js),
+    object_attribute_value(Js, students, Ss),
+    object_attribute_value(Js, minimumTime, MinimumUsefulTime),
+    object_attribute_value(Js, destinations, Destinations),
+    json_to_list_of_trips(Jf, Trips),
+    json_to_list_of_students(Ss, Students).
---
+++ source/sicstus/main.pl
@@ -0,0 +1,13 @@
+:- consult('./data-structures.pl').
+
+main :-
+    read_data(Data),
+    print(Data), nl.
+
+% student("budapest", ["05/05/2022", "06/05/2022"], 1, 600, "05:30:00", "23:30:00")
+% trip("wien", "zagreb", "09/05/2022, 10:10:00", "09/05/2022, 10:55:00", 45, 212, 0)
+
+print_elements([]) :- !.
+print_elements([H | T]) :-
+    print(H), nl,
+    print_elements(T).
