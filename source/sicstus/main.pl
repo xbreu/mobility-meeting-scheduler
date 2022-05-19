b9bed124a258e9f3e0fc57518a998fb769371d78
Alexandre Abreu <up201800168@up.pt>
2022-05-19 22:56:07 +0100

    add: creation of plan variables

--- source/sicstus/main.pl
+++ source/sicstus/main.pl
@@ -2,7 +2,10 @@
 
 main :-
     read_data(Data),
-    print(Data), nl.
+    data_students(Data, Students),
+    length(Students, N),
+    create_plans(N, Plans),
+    print(Plans).
 
 % student("budapest", ["05/05/2022", "06/05/2022"], 1, 600, "05:30:00", "23:30:00")
 % trip("wien", "zagreb", "09/05/2022, 10:10:00", "09/05/2022, 10:55:00", 45, 212, 0)
