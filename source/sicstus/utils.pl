a9f38a475711732a3d8b0da6ff86ccf7d3fe2b0b
Alexandre Abreu <up201800168@up.pt>
2022-05-30 18:09:10 +0100

    add: restrictions and timestamp on output

--- source/sicstus/utils.pl
+++ source/sicstus/utils.pl
@@ -12,3 +12,20 @@ indices_access(_, [], []).
 indices_access(List, [Index | Indices], [Value | Values]) :-
     element(Index, List, Value),
     indices_access(List, Indices, Values).
+
+% True if FlatList is a non-nested version of NestedList.
+% flatten(+NestedList, -FlatList)
+flatten(List, FlatList) :-
+    flatten(List, [], FlatList0),
+    !,
+    FlatList = FlatList0.
+
+flatten(Var, Tl, [Var|Tl]) :-
+    var(Var),
+    !.
+flatten([], Tl, Tl) :- !.
+flatten([Hd|Tl], Tail, List) :-
+    !,
+    flatten(Hd, FlatHeadTail, List),
+    flatten(Tl, Tail, FlatHeadTail).
+flatten(NonList, Tl, [NonList|Tl]).
